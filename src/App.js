23c0d98934c6f39c72cad170ff5eeaefaa1e58ed
sunny vamja <sunnyvamja4646@gmail.com>
2023-03-06 19:04:21 +0530

    commited by chiller

--- src/App.js
+++ src/App.js
@@ -1,4 +1,10 @@
-import { BrowserRouter, Routes, Route, Navigate, Router } from "react-router-dom";
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  Navigate,
+  Router,
+} from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Login from "./Component/Login";
 import Register from "./Component/Register";
@@ -15,40 +21,40 @@ import "./App.css";
 function App() {
   return (
     <BrowserRouter>
-    {/* Protected Routing */}
+      {/* Protected Routing */}
       {localStorage.getItem("login") != null ? <Profile /> : <></>}
       <Routes>
-        {!localStorage.getItem("login") ? 
+        {!localStorage.getItem("login") ? (
           <>
             <Route path="/Login" element={<Login />}></Route>
             <Route path="/Register" element={<Register />}></Route>
             <Route path="*" element={<Navigate to="/Login" />}></Route>
           </>
-          :
+        ) : (
           <>
             <Route path="/Dashboard" element={<Dashboard />} />
             <Route path="/Dashboard">
               <Route path="Student">
                 <Route path="Form" element={<Students />}></Route>
                 <Route path="Form/:id" element={<Students />}></Route>
-                <Route path='Table' element={<StudentTable />}></Route>
+                <Route path="Table" element={<StudentTable />}></Route>
               </Route>
 
               <Route path="User">
                 <Route path="Form" element={<User />}></Route>
                 <Route path="Form/:id" element={<User />}></Route>
-                <Route path='Table' element={<UserTable />}></Route>
+                <Route path="Table" element={<UserTable />}></Route>
               </Route>
 
               <Route path="Product">
-                <Route path="Form" element={<Product/>}></Route>
+                <Route path="Form" element={<Product />}></Route>
                 <Route path="Form/:id" element={<Product />}></Route>
-                <Route path='Table' element={<ProductTable />}></Route>
+                <Route path="Table" element={<ProductTable />}></Route>
               </Route>
             </Route>
-            <Route to="/Dashboard" element={<Dashboard/>}></Route>
+            <Route to="/Dashboard" element={<Dashboard />}></Route>
           </>
-        }
+        )}
       </Routes>
     </BrowserRouter>
   );
